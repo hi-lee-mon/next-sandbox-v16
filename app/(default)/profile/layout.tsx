35cc6d58d2c1ca888ee68b2de47bcabe89611450
hi-lee-mon <shunmel.09@gmail.com>
2026-05-09 12:29:53 +0900

    feat: プロフィールページのセッション確認機能を追加
    Layoutコンポーネントにセッション確認を追加し、未認証の場合はunauthorizedを表示するように変更。プロフィールページからセッション確認のロジックを削除し、コードを簡潔にしました。また、Imageコンポーネントとserver-functionに関する新しいドキュメントを追加しました。

--- app/(default)/profile/layout.tsx
+++ app/(default)/profile/layout.tsx
@@ -1,8 +1,15 @@
 import { health } from "@/app/_data/health"
-import { forbidden } from "next/navigation"
+import { verifySession } from "@/lib/verify-session";
+import { forbidden, unauthorized } from "next/navigation"
 
 export default async function Layout(props: LayoutProps<"/profile">) {
   health("profile-layout")
+  const session = await verifySession();
+
+  if (!session) {
+    // app/unauthorized.tsxを表示するnextの実験的機能
+    unauthorized();
+  }
   return (
     <>{props.children}</>
   )
--- app/(default)/profile/page.tsx
+++ app/(default)/profile/page.tsx
@@ -1,14 +1,6 @@
-import { verifySession } from "@/lib/verify-session";
 import Link from "next/link";
-import { unauthorized } from "next/navigation";
 
 export default async function Page() {
-  const session = await verifySession();
-
-  if (!session) {
-    // app/unauthorized.tsxを表示するnextの実験的機能
-    unauthorized();
-  }
   return (
     <div>プロフィール
       <br />
